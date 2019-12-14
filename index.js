0aeff088639bb362a914f8984b4f099a0f49e456
pprg1996 <pprg1996@gmail.com>
2019-12-14 06:24:48 -0400

    Navegacion interna

--- index.js
+++ index.js
@@ -19,3 +19,27 @@ menuBotton.addEventListener("click", () => {
     menuBotton.querySelector("img").setAttribute("src", "./assets/close.png");
   } else menuBotton.querySelector("img").setAttribute("src", "./assets/menu.png");
 });
+
+// Navegacion
+const navButtons = document.querySelectorAll(".nav-link");
+navButtons.forEach(navButton => {
+  navButton.addEventListener("click", e => {
+    e.preventDefault();
+
+    if (e.target.innerText === "Home") {
+      scrollTo(0, 0);
+    } else if (e.target.innerText === "Curso y Recuperación Escolar") {
+      let y = document.querySelector(".lista").offsetTop - 105;
+      scrollTo(0, y);
+    } else if (e.target.innerText === "Taller de Conversación") {
+      let y = document.querySelector(".conversacion").offsetTop - 85;
+      scrollTo(0, y);
+    } else if (e.target.innerText === "TOELF") {
+      let y = document.querySelector(".toelf").offsetTop - 85;
+      scrollTo(0, y);
+    } else if (e.target.innerText === "Contacto") {
+      let y = document.querySelector(".contacto").offsetTop - 80;
+      scrollTo(0, y);
+    }
+  });
+});
